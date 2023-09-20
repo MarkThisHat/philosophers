5dba53bc458aa91db73bcb3fa9edfa9edf857e8f
Marcos <marcos.chiu@gmail.com>
2023-09-20 18:13:27 -0300

    feat: assured numeric input

--- philo/srcs/args.c
+++ philo/srcs/args.c
@@ -6,7 +6,7 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 11:42:56 by maalexan          #+#    #+#             */
-/*   Updated: 2023/09/19 22:30:28 by maalexan         ###   ########.fr       */
+/*   Updated: 2023/09/20 18:10:44 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,27 @@ static t_bool	invalid_arguments(char *program_name)
 	return (FALSE);
 }
 
+static t_bool	check_digit_inputs(char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		while (ft_isdigit(*argv[i]))
+			argv[i]++;
+		if (*argv[i])
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
 t_bool	validate_args(int argc, char **argv)
 {
 	if (argc < 5 || argc > 6)
+		return (invalid_arguments(argv[0]));
+	if (!check_digit_inputs(argv + 1))
 		return (invalid_arguments(argv[0]));
 	return (TRUE);
 }
