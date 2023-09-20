d8ba1223044346061d0cf6fbebec8dbbcb9f6c3e
Marcos <marcos.chiu@gmail.com>
2023-09-19 22:32:31 -0300

    style: norminette

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,15 +6,15 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 11:41:33 by maalexan          #+#    #+#             */
-/*   Updated: 2023/09/19 22:30:22 by maalexan         ###   ########.fr       */
+/*   Updated: 2023/09/19 22:32:11 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    if (!validate_args(argc, argv))
-        return (1);
-    printf("%i\n", ft_atoui(argv[1]));
+	if (!validate_args(argc, argv))
+		return (1);
+	printf("%i\n", ft_atoui(argv[1]));
 }
