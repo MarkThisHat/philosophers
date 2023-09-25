9bf680f6c64a96ef59467693f4abd7fafb10245d
Marcos Chiu <marcos.chiu@gmail.com>
2023-09-25 10:48:10 -0300

    calculating

--- philo/srcs/existentialism.c
+++ philo/srcs/existentialism.c
@@ -6,7 +6,7 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 16:06:47 by maalexan          #+#    #+#             */
-/*   Updated: 2023/09/24 12:22:24 by maalexan         ###   ########.fr       */
+/*   Updated: 2023/09/25 10:23:09 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,26 @@ void	have_dinner(t_gazer *beholder, int base_beat)
 	printf("Base beat: %i\n", base_beat);
 	printf("Time to die %lli\n", beholder->die);
 	printf("Last meal: %lli\n", get_time_current(beholder->philos[0]->last_meal));
+	printf("Time alive left: %i\n", (int)(beholder->die - get_time_current(beholder->philos[0]->last_meal)));
+	usleep(base_beat);
+	printf("Last meal: %lli\n", get_time_current(beholder->philos[0]->last_meal));
+	printf("Time alive left: %i\n", (int)(beholder->die - get_time_current(beholder->philos[0]->last_meal)));
+	usleep(base_beat);
+	printf("Last meal: %lli\n", get_time_current(beholder->philos[0]->last_meal));
+	printf("Time alive left: %i\n", (int)(beholder->die - get_time_current(beholder->philos[0]->last_meal)));
+	usleep(base_beat);
+	printf("Last meal: %lli\n", get_time_current(beholder->philos[0]->last_meal));
+	printf("Time alive left: %i\n", (int)(beholder->die - get_time_current(beholder->philos[0]->last_meal)));
+	usleep(base_beat);
+	printf("Last meal: %lli\n", get_time_current(beholder->philos[0]->last_meal));
+	printf("Time alive left: %i\n", (int)(beholder->die - get_time_current(beholder->philos[0]->last_meal)));
+	usleep(base_beat);
+	printf("Last meal: %lli\n", get_time_current(beholder->philos[0]->last_meal));
+	printf("Time alive left: %i\n", (int)(beholder->die - get_time_current(beholder->philos[0]->last_meal)));
+	usleep(base_beat);
+	printf("Last meal: %lli\n", get_time_current(beholder->philos[0]->last_meal));
+	printf("Time alive left: %i\n", (int)(beholder->die - get_time_current(beholder->philos[0]->last_meal)));
+	usleep(base_beat);
+	printf("Last meal: %lli\n", get_time_current(beholder->philos[0]->last_meal));
+	printf("Time alive left: %i\n", (int)(beholder->die - get_time_current(beholder->philos[0]->last_meal)));
 }
