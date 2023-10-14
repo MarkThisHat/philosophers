c63759d998fec39329b6f50d74fba4d999f75ba7
Marcos <marcos.chiu@gmail.com>
2023-10-13 21:38:21 -0300

    fix: dumb sem

--- philo_bonus/srcs/nihilism.c
+++ philo_bonus/srcs/nihilism.c
@@ -6,11 +6,19 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 11:41:33 by maalexan          #+#    #+#             */
-/*   Updated: 2023/10/13 20:25:51 by maalexan         ###   ########.fr       */
+/*   Updated: 2023/10/13 21:37:49 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers_bonus.h"
+
+void	clear_sem(void)
+{
+	sem_unlink("/forks");
+	sem_unlink("/print");
+	sem_unlink("/end");
+	sem_unlink("/philo_done");
+}
 
 t_gazer	*get_observer(void)
 {
@@ -23,6 +31,7 @@ int	main(int argc, char **argv)
 {
 	if (!validate_args(argc, argv))
 		return (1);
+	clear_sem();
 	if (!set_philosophers(argc, argv))
 		return (1);
 	forking_it(get_observer());
